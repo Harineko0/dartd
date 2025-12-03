d1bb99bd24625df6446ad2a85cba04b7b0784453
Harineko0 <harineko0927@gmail.com>
2025-12-03 14:31:58 +0900

    format code

--- test/resources/class_provider/used/lib/main.dart
+++ test/resources/class_provider/used/lib/main.dart
@@ -4,7 +4,7 @@ import 'provider.dart';
 
 void main() {
   final container = ProviderContainer();
-  final state = container
-      .read(classProviderProvider.select((value) => value.name));
+  final state =
+      container.read(classProviderProvider.select((value) => value.name));
   print(state);
 }
